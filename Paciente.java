e013409b3195f00950049401dee265ce357d196e
Juan Esteban <Hoyosjuan979@gmail.com>
2024-11-11 15:39:26 -0500

    second commit

--- Paciente.java
+++ Paciente.java
@@ -1,17 +1,17 @@
 public class Paciente extends Persona {
 
-private String Codigo;
-
-public Paciente (String Nombre, String Documento, String Telefono, String Direccion, String Correo){
-    super(Nombre,Documento,Telefono, Direccion, Correo );
-}
-
-public String getCodigo() {
-    return Codigo;
-}
-
-public void setCodigo(String codigo) {
-    Codigo = codigo;
-}
-
+    private String Codigo;
+    
+    public Paciente (String Nombre, String Documento, String Telefono, String Direccion, String Correo){
+        super(Nombre,Documento,Telefono, Direccion, Correo );
+    }
+    
+    public String getCodigo() {
+        return Codigo;
+    }
+    
+    public void setCodigo(String codigo) {
+        Codigo = codigo;
+    }
+    
 }
